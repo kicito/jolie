c7bf70fee64eb36459fbf40c497927ec3b075ed7
Kie <narongrit.kie@gmail.com>
2019-11-22 14:50:10 +0100

    add README

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ See [this page](https://jolie-lang.org/downloads.html) for installation instruct
 
 [![Language grade: Java](https://img.shields.io/lgtm/grade/java/g/jolie/jolie.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/jolie/jolie/context:java) ![Travis-CI](https://travis-ci.org/jolie/jolie.svg?branch=master)
 
+## Jolie Module support
+
+This branch consist of development of Module system support for Jolie 2.0.
+
+## Task List
+
+- [ ] Adding `import` keyword. See syntax below.
+- [ ] modify `service` keyword to be main execution block of a jolie application
+
+### Import syntax
+
+```BNF
+import_stmt ::= "import" import_clause from_clause
+import_clause ::= namespace_import | import_list
+namespace_import ::= * "as" import_binding
+import_list ::= import_specifier | import_list , import_specifier
+import_specifier ::= import_binding | identifier_name as import_binding
+import_binding ::= binding_identifier
+from_clause ::= "from" module_specifier
+module_specifier ::= string_literal
+```
--- libjolie/src/test/java/jolie/lang/parse/TestModule.java
+++ libjolie/src/test/java/jolie/lang/parse/TestModule.java
@@ -1,8 +1,18 @@
 package jolie.lang.parse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
+import jolie.lang.parse.ast.Program;
+import jolie.lang.parse.util.ParsingUtils;
 
 public class TestModule
 {
@@ -12,5 +22,27 @@ public class TestModule
     void addsTwoNumbers()
     {
         assertEquals( 2, 1 + 2, "1 + 1 should equal 2" );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"import a from \"A\""})
+    void testImportStatements( String code ) throws IOException
+    {
+        InputStream targetStream = new ByteArrayInputStream( code.getBytes() );
+        try{
+            OLParser olParser = new OLParser(
+                new Scanner( targetStream, new URI("testImportStatements"), StandardCharsets.UTF_8.name(),
+                        false ),
+                new String[] {"dist/jolie/include"}, TestModule.class.getClassLoader() );
+            Program p = olParser.parse();
+            
+            System.out.println(p);
+        }catch( Exception e) {
+            e.printStackTrace();
+        }finally{
+            targetStream.close();
+        }
+
+
     }
 }
