d1c503bf5d65bbae493e711798f8d67ad61f1b48
kas0079 <kaspokumu@gmail.com>
2025-03-03 23:32:04 +0100

    large parts of AstService done

--- javaServices/ast/src/main/java/joliex/meta/AstService.java
+++ javaServices/ast/src/main/java/joliex/meta/AstService.java
@@ -26,16 +26,15 @@ import jolie.lang.parse.ParserException;
 import jolie.lang.parse.SemanticVerifier;
 import jolie.lang.parse.ast.*;
 import jolie.lang.parse.ast.expression.ConstantStringExpression;
-import jolie.lang.parse.ast.expression.SumExpressionNode;
 import jolie.lang.parse.ast.expression.VariableExpressionNode;
 import jolie.lang.parse.ast.types.*;
+import jolie.lang.parse.ast.types.refinements.*;
 import jolie.lang.parse.module.ModuleException;
 import jolie.lang.parse.util.ParsingUtils;
 import jolie.cli.CommandLineParser;
 
 import jolie.runtime.JavaService;
 import jolie.runtime.Value;
-import jolie.runtime.ValueVector;
 import jolie.runtime.embedding.java.JolieNative;
 import joliex.meta.spec.types.*;
 import joliex.meta.spec.types.Module;
@@ -45,9 +44,7 @@ import java.io.*;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class AstService extends JavaService {
 
@@ -90,9 +87,7 @@ public class AstService extends JavaService {
 	private List<ServiceDef> parseServices(Program module) {
 		return module.children().stream()
 			.filter( child -> child instanceof ServiceNode )
-			.map( node -> {
-				return (ServiceNode) node;
-			} )
+			.map( node -> (ServiceNode) node )
 			.map( this::serviceToValue
 			)
 			.toList();
@@ -101,8 +96,22 @@ public class AstService extends JavaService {
 		return List.of();
 	}
 
-	private List<InterfaceDef> parseInterface(Program module) {
+	private List<InterfaceDef> parseInterfaces(Program module) {
+		//TODO consider storing the list of interfaces to easily distinguish between local vs imported interfaces
+		return module.children().stream()
+			.filter( child -> child instanceof InterfaceDefinition )
+			.map( node -> (InterfaceDefinition) node )
+			.map( this::getInterface )
+			.toList();
+	}
 
+	private InterfaceDef getInterface(InterfaceDefinition interfaceDefinition) {
+		LocatedString name = new LocatedString( interfaceDefinition.name(), location(interfaceDefinition) );
+		return InterfaceDef.builder()
+			.name( name )
+			.textLocation( location( interfaceDefinition ) )
+			.operations( getOperations( interfaceDefinition ) )
+			.build();
 	}
 
 	private ServiceDef serviceToValue(ServiceNode service) {
@@ -121,18 +130,47 @@ public class AstService extends JavaService {
 			builder.documentation( documentation );
 		}
 
-		for(OLSyntaxNode node : service.program().children()) {
-			if(node instanceof InputPortInfo ) {
-				InputPortInfo port = (InputPortInfo) node;
-			} else if( node instanceof OutputPortInfo ) {
-				OutputPortInfo port = (OutputPortInfo) node;
+		List<InputPort> inputPorts = service.program().children().stream()
+			.filter( node -> node instanceof InputPortInfo )
+			.map(node -> (InputPortInfo) node )
+			.map( this::createInputPort )
+			.toList();
 
-			}
-		}
-		builder.inputPorts(  )
-			.outputPorts(  );
+		List<OutputPort> outputPorts = service.program().children().stream()
+			.filter( node -> node instanceof OutputPortInfo )
+			.map(node -> (OutputPortInfo) node )
+				.map( this::createOutputPort )
+					.toList();
 
-		return ServiceDef.toValue( )
+		builder.inputPorts( inputPorts )
+				.outputPorts( outputPorts );
+
+		return builder.build();
+	}
+
+	private OutputPort createOutputPort(OutputPortInfo outputPortInfo) {
+		OutputPort.Builder builder = OutputPort.builder()
+			.textLocation( location( outputPortInfo ) );
+
+		// FIXME ImportableSymbol::name is just a string so it has no location
+		LocatedString name = new LocatedString( outputPortInfo.id(), location(outputPortInfo) );
+		builder.name( name );
+
+		// might be too simple
+		ConstantStringExpression locationExpression = (ConstantStringExpression) outputPortInfo.location();
+		LocatedString location = new LocatedString( locationExpression.value(), location(locationExpression) );
+		builder.location( location );
+
+		// FIXME probably buggy
+		VariableExpressionNode variableExpressionNode = (VariableExpressionNode) outputPortInfo.protocol();
+		ConstantStringExpression protocolExpression = (ConstantStringExpression) variableExpressionNode.variablePath().path().getFirst().key();
+		LocatedString protocol = new LocatedString( protocolExpression.value(), location(protocolExpression));
+		builder.protocol( protocol );
+
+		return builder
+			.interfaces( getInterfaces( outputPortInfo ) )
+			.operations( getOperations( outputPortInfo ) )
+			.build();
 	}
 
 	private InputPort createInputPort(InputPortInfo inputPortInfo) {
@@ -152,14 +190,30 @@ public class AstService extends JavaService {
 		ConstantStringExpression protocolExpression = (ConstantStringExpression) variableExpressionNode.variablePath().path().getFirst().key();
 		LocatedString protocol = new LocatedString( protocolExpression.value(), location(protocolExpression));
 
-
-		inputPortInfo.redirectionMap()
-		builder.location( location )
+		return builder.location( location )
 			.protocol( protocol )
 			.redirections( getRedirections( inputPortInfo ) )
 			.interfaces( getInterfaces( inputPortInfo ) )
 			.operations( getOperations( inputPortInfo ) )
-			.aggregations(  )
+			.aggregations( getAggregations( inputPortInfo ) )
+			.build();
+	}
+
+	private List<Aggregation> getAggregations(InputPortInfo inputPort) {
+		return Arrays.stream( inputPort.aggregationList() )
+			.map( x -> getAggregation( x ) )
+			.toList();
+	}
+
+	private Aggregation getAggregation( InputPortInfo.AggregationItemInfo aggregationItemInfo ) {
+		throw new NotImplementedException("tbd");
+		return Aggregation.builder()
+			//FIXME aggregationItemInfo doesn't have a ParsingContext
+			.textLocation( null )
+			//FIXME aggregations can have multiple ports (a collection), but not multiple extenders, so it should be [0,1]
+			.outputPort( null )
+			.extender( null )
+			.build();
 	}
 
 	private String getSymbolRef(ImportableSymbol node) {
@@ -174,7 +228,6 @@ public class AstService extends JavaService {
 			.map( entry -> {
 				/*
 					FIXME InputPortInfo::redirectionMap has no ParsingContext, but we should be able to store a Map<String, OutputPort> and get them through that.
-				    But if we can find it anyway maybe Redirection should just save the OutputPort instead.
 				*/
 				LocatedSymbolRef outputPort = new LocatedSymbolRef( entry.getValue(), location(inputPort) );
 				LocatedString name = new LocatedString( entry.getKey(), location(inputPort) );
@@ -186,29 +239,62 @@ public class AstService extends JavaService {
 	private List<String> getInterfaces(PortInfo port) {
 		return port.getInterfaceList().stream()
 			.map( InterfaceDefinition::name)
+			//TODO add logic to look through imports if the interface is not defined in the same module.
 			.toList();
 	}
 
 	private List<Operation> getOperations(OperationCollector operationCollector) {
 		return operationCollector.operationsMap()
-			.entrySet()
+			//the operation name is also stored in OperationDeclaration, so we don't need the key.
+			.values()
 			.stream()
-			.map((entry) -> {
-
-			})
+			.map(this::getOperation)
 			.toList();
 	}
 
 	private Operation getOperation(OperationDeclaration operationDeclaration) {
-		if( operationDeclaration instanceof OneWayOperationDeclaration) {
-			OneWayOperationDeclaration oneWayOperationDeclaration = (OneWayOperationDeclaration) operationDeclaration;
-			TypeDefinition requestType = oneWayOperationDeclaration.requestType();
-			OneWayOperation ow = new OneWayOperation(  );
-		} else if( operationDeclaration instanceof RequestResponseOperationDeclaration) {
+		return switch( operationDeclaration ) {
+			case OneWayOperationDeclaration oneWayOperationDeclaration -> {
+				yield Operation.of1( getOneWayOperation( oneWayOperationDeclaration ) );
+			}
+			case RequestResponseOperationDeclaration requestResponseOperationDeclaration -> {
+				yield Operation.of2( getRequestResponseOperation( requestResponseOperationDeclaration ) );
+			}
+			case null, default ->
+				throw new NotImplementedException("operation: " + operationDeclaration +
+					" is neither a OneWayOperationDeclaration or a RequestResponseOperationDeclaration");
+		};
+	}
 
-		} else {
-			throw new NotImplementedException("operation: " + operationDeclaration + " is neither a OneWayOperationDeclaration or a RequestResponseOperationDeclaration");
-		}
+	private OneWayOperation getOneWayOperation(OneWayOperationDeclaration oneWayOperationDeclaration) {
+		return OneWayOperation.builder()
+			.textLocation( location(oneWayOperationDeclaration) )
+			.requestType( getType( oneWayOperationDeclaration.requestType() ) )
+			//FIXME name location
+			.name( new LocatedString( oneWayOperationDeclaration.id(), location(oneWayOperationDeclaration) ) )
+			.build();
+
+	}
+
+	private RequestResponseOperation getRequestResponseOperation(RequestResponseOperationDeclaration requestResponseOperationDeclaration) {
+		return RequestResponseOperation.builder()
+			.textLocation( location(requestResponseOperationDeclaration) )
+			.responseType( getType( requestResponseOperationDeclaration.responseType() ) )
+			.requestType( getType( requestResponseOperationDeclaration.requestType() ) )
+			//FIXME name location
+			.name( new LocatedString( requestResponseOperationDeclaration.id(), location(requestResponseOperationDeclaration) ) )
+			.faults( requestResponseOperationDeclaration
+				.faults()
+				.entrySet()
+				.stream()
+				.map( entry -> {
+					Location location = location(requestResponseOperationDeclaration);
+					LocatedString locatedName = new LocatedString(entry.getKey(), location);
+					Type faultType = getType( entry.getValue() );
+					return new FaultType(location, locatedName, faultType );
+				} )
+				.toList())
+			.build();
 	}
 
 	private Type getType(TypeDefinition typeDefinition) {
@@ -224,12 +310,14 @@ public class AstService extends JavaService {
 					new Type.S3(getTypeReference( typeDefinitionLink ))
 				);
 			}
-			//case TypeDefinitionUndefined u -> u; // Can't be shown in ast.ol types InlineDefinition
+			// TODO implement undefined type in ast.ol
+			case TypeDefinitionUndefined u -> throw new NotImplementedException("Type undefined is not supported yet" + typeDefinition); // Can't be shown in ast.ol types InlineDefinition
 			case TypeInlineDefinition typeInlineDefinition -> {
 				return Type.of1(
 					new Type.S1( getTreeType(typeInlineDefinition) )
 				);
 			}
+			default -> throw new IllegalStateException("Unexpected value: " + typeDefinition);
 		}
 	}
 
@@ -247,11 +335,61 @@ public class AstService extends JavaService {
 	}
 
 	private TreeType getTreeType(TypeInlineDefinition typeInlineDefinition) {
-		TreeType.Builder builder = TreeType.builder()
-			.textLocation( location(typeInlineDefinition) )
-			.basicType( getBasicType(typeInlineDefinition.basicType()) );
+
+		List<TreeNodeType> nodes = typeInlineDefinition
+			.subTypes()
+			.stream()
+			.map( Map.Entry::getValue )
+			.map( this::getTreeNodeType )
+			.toList();
+
+		TreeType.Builder builder = TreeType.builder();
+		Optional<String> documentationString = typeInlineDefinition.getDocumentation();
+		if(documentationString.isPresent()) {
+			// FIXME DocumentedNode::Documentation is just a string so it has no location
+			Documentation documentation = new Documentation(documentationString.get(), location(typeInlineDefinition));
+			builder.documentation( documentation );
+		}
+		return builder.textLocation( location(typeInlineDefinition) )
+			.basicType( getBasicType(typeInlineDefinition.basicType()) )
+			.nodes( nodes )
+			.build();
 
 	}
+
+	private TreeNodeType getTreeNodeType(TypeDefinition typeDefinition) {
+		TreeNodeType.Builder builder = TreeNodeType.builder();
+		Optional<String> documentationString = typeDefinition.getDocumentation();
+		if(documentationString.isPresent()) {
+			// FIXME DocumentedNode::Documentation is just a string so it has no location
+			Documentation documentation = new Documentation(documentationString.get(), location(typeDefinition));
+			builder.documentation( documentation );
+		}
+		return builder
+			.textLocation( location( typeDefinition ) )
+			.name( new LocatedString( typeDefinition.name(), location( typeDefinition ) ) )
+			.range( getRange(typeDefinition) )
+			.type(getType( typeDefinition ))
+			.build();
+
+	}
+
+	private NonNegativeIntRange getRange(TypeDefinition typeDefinition) {
+		return NonNegativeIntRange.builder()
+			.min( typeDefinition.cardinality().min() )
+			.max( typeDefinition.cardinality().max() )
+			.build();
+	}
+
+	/*
+	private Optional<Documentation> getDocumentation( DocumentedNode node ) {
+		return node.getDocumentation()
+			.flatMap( documentation -> {
+				// FIXME DocumentedNode and its documentation have no ParsingContext.
+				return new Documentation( documentation, null );
+			} );
+	}
+	 */
 
 	private BasicType getBasicType(BasicTypeDefinition basicTypeDefinition) {
 
@@ -263,7 +401,7 @@ public class AstService extends JavaService {
 			case DOUBLE -> BasicType.of5( getDoubleBasicType( basicTypeDefinition ) );
 			case VOID -> BasicType.of1( getVoidBasicType() );
 			case ANY, RAW -> {
-				// FIXME not supported in ast.ol
+				// TODO add types in ast.ol
 				throw new NotImplementedException("no corresponding type in ast.ol");
 			}
 			case null, default -> {
@@ -276,23 +414,98 @@ public class AstService extends JavaService {
 
 	private StringBasicType getStringBasicType(BasicTypeDefinition basicTypeDefinition) {
 
+		List<StringRefinement> stringRefinements = basicTypeDefinition.refinements().stream()
+			.map( basicTypeRefinement -> switch( basicTypeRefinement ) {
+				case BasicTypeRefinementStringLength length -> {
+					IntRange range = new IntRange( length.getMin(), length.getMax() );
+					yield StringRefinement.of1(
+						new StringRefinement.S1( range )
+					);
+				}
+				case BasicTypeRefinementStringList list -> {
+					List<String> enumeration = list.getList();
+					yield StringRefinement.of2(new StringRefinement.S2( enumeration ) );
+				}
+				case BasicTypeRefinementStringRegex regex -> {
+					yield StringRefinement.of3( new StringRefinement.S3( regex.getRegex() ) );
+				}
+				default -> throw new IllegalStateException("Unexpected value: " + basicTypeRefinement);
+			} )
+			.toList();
+		return new StringBasicType( new JolieNative.JolieVoid(), stringRefinements );
 	}
 
-	private IntBasicType getIntBasicType(BasicTypeDefinition basicTypeDefinition) {
 
+
+	private IntBasicType getIntBasicType(BasicTypeDefinition basicTypeDefinition) {
+		/* TODO can we ever have more than one BasicTypeRefinementIntegerRanges? if not, we can simplify into:
+		List<IntRefinement> intRefinements = new ArrayList<>();
+		if (!basicTypeDefinition.refinements().isEmpty() ) {
+			BasicTypeRefinementIntegerRanges refinement =
+				(BasicTypeRefinementIntegerRanges) basicTypeDefinition.refinements().get( 0 );
+			List<IntRange> intRanges = refinement
+				.getRanges()
+				.stream()
+				.map( interval -> {
+					return new IntRange( interval.getMin(), interval.getMax() );
+				} ).toList();
+			intRefinements.add( new IntRefinement( intRanges ) );
+		}
+		*/
+		List<IntRefinement> intRefinements = basicTypeDefinition.refinements().stream()
+			// should be safe as this is the only possible refinement.
+			.map(refinement -> (BasicTypeRefinementIntegerRanges) refinement)
+			.map( refinement -> {
+				List<IntRange> intRanges = refinement
+					.getRanges()
+					.stream()
+					.map(interval -> new IntRange( interval.getMin(), interval.getMax() ) )
+					.toList();
+				return new IntRefinement( intRanges );
+			} )
+			.toList();
+
+
+		return new IntBasicType( new JolieNative.JolieVoid(), intRefinements);
 	}
 
 	private LongBasicType getLongBasicType(BasicTypeDefinition basicTypeDefinition) {
-
+		List<LongRefinement> longRefinements = basicTypeDefinition.refinements().stream()
+			.map(refinement -> {
+				// should be safe as this is the only possible refinement.
+				return (BasicTypeRefinementLongRanges) refinement;
+			})
+			.map( refinement -> {
+				List<LongRange> longRanges = refinement
+					.getRanges()
+					.stream()
+					.map(interval -> new LongRange( interval.getMin(), interval.getMax() ) )
+					.toList();
+				return new LongRefinement( longRanges );
+			} )
+			.toList();
+		return new LongBasicType( new JolieNative.JolieVoid(), longRefinements );
 	}
 	private BoolBasicType getBoolBasicType() {
 		return new BoolBasicType( new JolieNative.JolieVoid() );
 
 	}
 	private DoubleBasicType getDoubleBasicType(BasicTypeDefinition basicTypeDefinition) {
-		basicTypeDefinition.refinements()
-
-		return new DoubleBasicType(  )
+		List<DoubleRefinement> doubleRefinements = basicTypeDefinition.refinements().stream()
+			.map(refinement -> {
+				// should be safe as this is the only possible refinement.
+				return (BasicTypeRefinementDoubleRanges) refinement;
+			})
+			.map( refinement -> {
+				List<DoubleRange> doubleRanges = refinement
+					.getRanges()
+					.stream()
+					.map(interval -> new DoubleRange( interval.getMin(), interval.getMax() ) )
+					.toList();
+				return new DoubleRefinement( doubleRanges );
+			} )
+			.toList();
+		return new DoubleBasicType( new JolieNative.JolieVoid(), doubleRefinements );
 	}
 	private VoidBasicType getVoidBasicType() {
 		return new VoidBasicType( new JolieNative.JolieVoid() );
@@ -300,7 +513,11 @@ public class AstService extends JavaService {
 	}
 
 
-
+	/**
+	 * Creates a Jolie type Location from an AST node.
+	 * @param node an AST node
+	 * @return The Location
+	 */
 	private Location location( OLSyntaxNode node ) {
 		jolie.lang.parse.context.Location nodeLocation = node.context().location();
 
@@ -327,7 +544,7 @@ public class AstService extends JavaService {
 		return Module.toValue(Module.builder()
 			.types( parseTypes(moduleProgram) )
 			.services(parseServices( moduleProgram ))
-			.interfaces( parseInterface(moduleProgram) )
+			.interfaces( parseInterfaces(moduleProgram) )
 			.build()
 		);
 	}
